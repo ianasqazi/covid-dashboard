f8bf7bfe3cbfc514f20db697fb77f788f715e628
Anas Qazi <ianasqazi@gmail.com>
2020-04-08 18:29:10 -0400

    new client app

--- server.js
+++ server.js
@@ -8,19 +8,24 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// const publicPath = path.join(__dirname,'client', 'public');
-// const buildPath = path.join(__dirname,'build', 'public');
+const publicPath = path.join(__dirname,'client', 'public');
+const buildPath = path.join(__dirname,'build', 'public');
 
 // app.use(express.static(publicPath));
 // app.use('/', routes);
 // app.use(express.static(path.join(__dirname, 'client/public')));
+app.get('/api/getList', (req,res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+});
 
-// app.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname+'/client/index.html'));
-// });
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
 
 // app.use(function(req, res) {
 //   if (process.env.NODE_ENV === 'production') {
