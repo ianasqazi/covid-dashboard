d6bebb46f177b437bf09ed7b2155cb558b5436db
Anas Qazi <ianasqazi@gmail.com>
2020-04-08 14:07:17 -0400

    adding new route

--- server.js
+++ server.js
@@ -16,20 +16,25 @@ const buildPath = path.join(__dirname,'build', 'public');
 
 // app.use(express.static(publicPath));
 // app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(function(req, res) {
-  if (process.env.NODE_ENV === 'production') {
-    console.log(buildPath);
-    // res.sendFile(path.join(__dirname, './client/build/index.html'));
-    res.sendFile(path.join(buildPath, 'index.html'));
-
-  } else {
-    // console.log(res);
-    // res.sendFile(path.join(__dirname, './client/public/index.html'));
-      res.sendFile(path.join(buildPath, 'index.html'));
-
-  }
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+// app.use(function(req, res) {
+//   if (process.env.NODE_ENV === 'production') {
+//     console.log(buildPath);
+//     // res.sendFile(path.join(__dirname, './client/build/index.html'));
+//     res.sendFile(path.join(buildPath, 'index.html'));
+
+//   } else {
+//     // console.log(res);
+//     // res.sendFile(path.join(__dirname, './client/public/index.html'));
+//       res.sendFile(path.join(publicPath, 'index.html'));
+
+//   }
+// });
 
 
 app.listen(port, () => 
