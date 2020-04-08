e2308b3e4def4584802ebceabfb3fb7a6a5e9826
Anas Qazi <ianasqazi@gmail.com>
2020-04-08 13:26:10 -0400

    errors

--- server.js
+++ server.js
@@ -19,34 +19,18 @@ const buildPath = path.join(__dirname,'build', 'public');
 
 app.use(function(req, res) {
   if (process.env.NODE_ENV === 'production') {
+    console.log(buildPath);
     // res.sendFile(path.join(__dirname, './client/build/index.html'));
-    res.sendFile(path.join(buildPath, 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
 
   } else {
-    // console.log('hit');
+    // console.log(res);
     // res.sendFile(path.join(__dirname, './client/public/index.html'));
       res.sendFile(path.join(publicPath, 'index.html'));
 
   }
 });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(publicPath, 'index.html'));
-// });
-
-// app.get('/api/hello', (req, res) => {
-//   res.send({ express: 'Hello From Express' });
-// });
-
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-    
-//   // Handle React routing, return all requests to React app
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
 
 app.listen(port, () => 
   console.log(`Server running on port ${port}`)
