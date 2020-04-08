d0028c0641b39697a282b338e82213352fc030f2
Anas Qazi <ianasqazi@gmail.com>
2020-04-08 14:20:18 -0400

    fixing errors

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const routes = require('./routes/index');
+// const routes = require('./routes/index');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-const publicPath = path.join(__dirname,'client', 'public');
-const buildPath = path.join(__dirname,'build', 'public');
+// const publicPath = path.join(__dirname,'client', 'public');
+// const buildPath = path.join(__dirname,'build', 'public');
 
 // app.use(express.static(publicPath));
 // app.use('/', routes);
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/public')));
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req,res) =>{
+//   res.sendFile(path.join(__dirname+'/client/index.html'));
+// });
 
 // app.use(function(req, res) {
 //   if (process.env.NODE_ENV === 'production') {
