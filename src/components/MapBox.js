ae061025385a9dc26458da3d17574075dda62530
Anas Qazi <ianasqazi@gmail.com>
2020-04-11 00:11:14 -0400

    error fix

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -64,59 +64,59 @@ function MapBox() {
         });
 
         // Add our layer
-        // map.addLayer({
-        //   id: "circles",
-        //   source: "points", // this should be the id of source
-        //   type: "circle",
-        //   paint: {
-        //     "circle-opacity": 0.75,
-        //     "circle-stroke-width": [
-        //       "interpolate",
-        //       ["linear"],
-        //       ["get", "cases"],
-        //       1,
-        //       1,
-        //       100000,
-        //       1.75
-        //     ],
-        //     "circle-radius": [
-        //       "interpolate",
-        //       ["linear"],
-        //       ["get", "cases"],
-        //       1,
-        //       4,
-        //       1000,
-        //       8,
-        //       4000,
-        //       10,
-        //       8000,
-        //       14,
-        //       12000,
-        //       18,
-        //       100000,
-        //       40
-        //     ],
-        //     "circle-color": [
-        //       "interpolate",
-        //       ["linear"],
-        //       ["get", "cases"],
-        //       1,
-        //       "#ffffb2",
-        //       5000,
-        //       "#fed976",
-        //       10000,
-        //       "#feb24c",
-        //       25000,
-        //       "#fd8d3c",
-        //       50000,
-        //       "#fc4e2a",
-        //       75000,
-        //       "#e31a1c",
-        //       100000,
-        //       "#b10026"
-        //     ]
-        //   }
-        // });
+        map.addLayer({
+          id: "circles",
+          source: "points", // this should be the id of source
+          type: "circle",
+          paint: {
+            "circle-opacity": 0.75,
+            "circle-stroke-width": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              1,
+              100000,
+              1.75
+            ],
+            "circle-radius": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              4,
+              1000,
+              8,
+              4000,
+              10,
+              8000,
+              14,
+              12000,
+              18,
+              100000,
+              40
+            ],
+            "circle-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              "#ffffb2",
+              5000,
+              "#fed976",
+              10000,
+              "#feb24c",
+              25000,
+              "#fd8d3c",
+              50000,
+              "#fc4e2a",
+              75000,
+              "#e31a1c",
+              100000,
+              "#b10026"
+            ]
+          }
+        });
 
         const popup = new mapboxgl.Popup({
           closeButton: false,
