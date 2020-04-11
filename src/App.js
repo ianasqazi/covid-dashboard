d2f9e1abd2be156d426645e3f4927e97fe1bc40a
Anas Qazi <ianasqazi@gmail.com>
2020-04-10 22:51:03 -0400

    installed swr

--- src/App.js
+++ src/App.js
@@ -192,32 +192,10 @@ function App() {
 
   return (
 
-<Router>
+    <Router>
       <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav> */}
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-
+          
           <Route path="/">
 
             <NavbarReact />
@@ -226,6 +204,7 @@ function App() {
 
 
             <Footer />
+
           </Route>
 
         </Switch>
@@ -233,18 +212,6 @@ function App() {
     </Router>
 
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 
